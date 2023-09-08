b8eb6528e0913f0b21affc1df8371603415ba3d4
ulukcodewise <uluk0822@gmail.com>
2023-09-07 21:54:00 -0500

    Commit

--- src/Day7/ManipulationPractice.java
+++ src/Day7/ManipulationPractice.java
@@ -13,10 +13,17 @@ public class ManipulationPractice {
 //        System.out.println(message);
 //        System.out.println(message.replace(" ","??"));
         Scanner scanner = new Scanner(System.in);
-        String result = scanner.nextLine();
+//        String result = scanner.nextLine();
 //        System.out.println(result.contains("@"));
-        int length = result.length();
-        System.out.println(length);
-        System.out.println(result.charAt(length-1));
+//        int length = result.length();
+//        System.out.println(length);
+//        System.out.println(result.charAt(length-1));
+        String word = scanner.nextLine();
+        String old = scanner.nextLine();
+        String newWord = scanner.nextLine();
+//        System.out.println(word.contains(old));
+//        System.out.println(word.equalsIgnoreCase(old));
+
+        System.out.println(word.replace(old, newWord));
     }
 }
