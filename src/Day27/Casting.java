96f5b10d42fbc8db237f60e4a5cdd6d5507b9ef7
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2023-10-16 14:29:48 -0500

    Create Casting.java

---
+++ src/Day27/Casting.java
@@ -0,0 +1,18 @@
+package Day27;
+
+public class Casting {
+    public static void main(String[] args) {
+        // Explicit casting - casting done by programmer
+        int intNumber = 5;
+        byte byteNumber = (byte) intNumber;
+
+        // Implicit casting - casting done by JVM
+        int intValue = 543543;
+        long longValue = intValue;
+
+
+         int number = (int)35.5;
+
+
+    }
+}
