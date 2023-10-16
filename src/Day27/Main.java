dc81860824416fc34d4d314d39d639bcae507d0d
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2023-10-16 14:31:32 -0500

    Create Main.java

---
+++ src/Day27/Main.java
@@ -0,0 +1,21 @@
+package Day27;
+
+public class Main {
+    public static void main(String[] args) {
+        int intNumber; // primitive variable
+        // autobox - convert
+        // Integer.parseInt() -> converts String to int
+
+
+
+
+        // Integer n = Integer.valueOf(5) -> hardcoded conversion or wrap
+        // Integer n = 5; -> autoConversion or autoboxing
+
+        Integer integer1 = Integer.valueOf(5);
+        Integer integer2 = 5;
+        System.out.println(integer1);
+        System.out.println(integer2);
+
+    }
+}
